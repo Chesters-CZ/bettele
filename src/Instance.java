4d72c434294ff68dcd3cd4d6ab4cc2ad9de7b710
Chesters-CZ <bezouskavit@gmail.com>
2021-04-19 15:35:06 +0200

    wtf já už nechci gitovat ideou

---
+++ src/Instance.java
@@ -0,0 +1,6 @@
+public class Instance {
+    public static Gaym gaymInstance = new Gaym();
+    public static Tiles tilesInstance = new Tiles();
+    public static Field fieldInstance = new Field();
+    public static Enemy enemyInstance = new Enemy();
+}
